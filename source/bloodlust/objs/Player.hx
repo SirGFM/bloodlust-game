7833fd606b0ba50ce724f011aa5c4aee63f7bb6e
SirGFM <gfmandaji@hotmail.com>
2022-10-09 12:35:58 -0300

    objs/Player: Track the player state
    Properly track the player's current state, and implement some simple state
    transition logic.

--- source/bloodlust/objs/Player.hx
+++ source/bloodlust/objs/Player.hx
@@ -7,6 +7,11 @@ import flixel.util.FlxColor;
 import bloodlust.utils.Input;
 import bloodlust.utils.GameMath;
 
+private enum PlayerState {
+	STAND;
+	WALK;
+}
+
 private typedef Point = {
 	x: Float,
 	y: Float,
@@ -19,11 +24,25 @@ class Player extends FlxSprite {
 
 	private var plgInput: Input;
 
+	private var _lastState: PlayerState;
+	private var _state: PlayerState;
+
 	override public function new() {
 		super();
 
 		this.makeGraphic(PLAYER_SIZE, PLAYER_SIZE, FlxColor.RED);
 		this.plgInput = FlxG.plugins.get(Input);
+
+		this._state = STAND;
+		this._lastState = STAND;
+	}
+
+	private function getNewState(): PlayerState {
+		if (this.plgInput.getAny([LEFT, RIGHT, UP, DOWN], PRESSED)) {
+			return WALK;
+		}
+
+		return STAND;
 	}
 
 	private function getRawDirection(): Point {
@@ -56,9 +75,20 @@ class Player extends FlxSprite {
 	}
 
 	override public function update(elapsed:Float) {
-		this.setWalkSpeed();
+		this._state = this.getNewState();
+
+		switch (this._state) {
+		case WALK:
+			this.setWalkSpeed();
+		case STAND:
+			this.velocity.scale(0);
+		default:
+			{ /* do nothing */ }
+		}
 
 		super.update(elapsed);
+
+		this._lastState = this._state;
 	}
 }
 
